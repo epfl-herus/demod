fea54b35146ff8fc1c803a5f25d844ae080db331
lionel42 <43442120+lionel42@users.noreply.github.com>
2021-05-05 10:08:28 +0200

    added new test

---
+++ tests/test_parse_helper.py
@@ -0,0 +1,27 @@
+from demod.utils.parse_helpers import (
+    states_to_transitions
+)
+import unittest
+
+import numpy as np
+
+class TestStatesToTransitions(unittest.TestCase):
+    def test_simple(self):
+        """Test a simple case with one transition."""
+        states = np.array([
+            [0, 0],
+            [0, 1]
+        ], dtype=int)
+        transition_dic = states_to_transitions(states)
+        self.assertTrue(np.all(
+            transition_dic['persons'] == np.array([1], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['times'] == np.array([1], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['new_states'] == np.array([1], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['old_states'] == np.array([0], dtype=int)
+        ))
