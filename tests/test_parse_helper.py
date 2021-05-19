c7315bf8f86861c3674b301ede8792a44fba39ea
lionel42 <43442120+lionel42@users.noreply.github.com>
2021-05-19 18:45:18 +0200

    added helpers for parsing semi marov tpms

--- tests/test_parse_helper.py
+++ tests/test_parse_helper.py
@@ -1,4 +1,6 @@
 from demod.utils.parse_helpers import (
+    counts_to_pdf,
+    states_to_tpms_with_durations,
     states_to_transitions,
     states_to_tpms
 )
@@ -90,4 +92,120 @@ class TestStatesToTransitions(unittest.TestCase):
         ))
         self.assertTrue(np.all(
             transition_dic['old_states'] == np.array([0, 1, 0, 0], dtype=int)
+        ))
+
+    def test_durations(self):
+        states = np.array([
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+        ], dtype=int)
+        transition_dic = states_to_transitions(states, return_duration=True)
+        print(transition_dic)
+        self.assertTrue(np.all(
+            transition_dic['persons'] == np.array([1, 1, 2, 2], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['times'] == np.array([0, 1, 1, 2], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['old_states'] == np.array([1, 0, 1, 0], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['new_states'] == np.array([0, 1, 0, 1], dtype=int)
+        ))
+        self.assertTrue(np.all(
+            transition_dic['durations'] == np.array([1, 2, 1, 2], dtype=int)
+        ))
+
+class TestCountsToPDF(unittest.TestCase):
+    def test_counts_to_pdf(self):
+        a = np.array([
+            [0, 3, 1],
+            [1, 2, 1]
+        ])
+        pdfs = counts_to_pdf(a)
+        self.assertTrue(
+            np.all(pdfs == np.array([
+                [0., 0.75, 0.25],
+                [0.25, 0.5, 0.25]
+            ]))
+        )
+
+    def test_0_replaces(self):
+        a = np.array([
+            [0, 0, 0],
+            [1, 2, 1]
+        ])
+        pdfs = counts_to_pdf(a)
+        self.assertTrue(
+            np.all(pdfs == np.array([
+                [1., 0., 0.],
+                [0.25, 0.5, 0.25]
+            ]))
+        )
+
+    def test_higher_shape(self):
+        a = np.array([
+            [
+                [0, 2, 2],
+                [1, 2, 1]
+            ], [
+                [0, 3, 1],
+                [1, 2, 1]
+            ],
+        ])
+        pdfs = counts_to_pdf(a)
+        self.assertTrue(
+            np.all(pdfs == np.array([
+                [
+                    [0., 0.5, 0.5],
+                    [0.25, 0.5, 0.25]
+                ], [
+                    [0., 0.75, 0.25],
+                    [0.25, 0.5, 0.25]
+                ],
+            ]))
+        )
+
+
+class TestTPMsWithDuration(unittest.TestCase):
+    def test_simple(self):
+        states = np.array([
+            [0, 0, 0],
+            [0, 1, 1],
+            [0, 1, 0],
+        ], dtype=int)
+        tpms, dur0, dur1 = states_to_tpms_with_durations(
+            states, first_tpm_modification_algo='nothing'
+        )
+        print(states_to_transitions(states, return_duration=True))
+        print(tpms, dur0, dur1)
+        self.assertTrue(np.all(
+            tpms == np.array([
+                [
+                    [1., 0.],
+                    [1., 0.]
+                ], [
+                    [0., 1.],
+                    [0., 1.]
+                ], [
+                    [1., 0.],
+                    [1., 0.]
+                ]
+            ])
+        ))
+        self.assertTrue(np.all(
+            dur0 == np.array([
+                [
+                    [0., 1., 0.],
+                    [1., 0., 0.]
+                ], [
+                    [1., 0., 0.],
+                    [0., 0.5, 0.5]
+                ], [
+                    [0., 0., 1.],
+                    [1., 0., 0.]
+                ]
+            ])
         ))
