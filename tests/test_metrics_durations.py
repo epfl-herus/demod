937ff49ddc6d976357c2a57b4109d41fa2d8d603
lionel42 <lionel.constantin@epfl.ch>
2021-06-23 15:22:52 +0200

    formatted

--- tests/test_metrics_durations.py
+++ tests/test_metrics_durations.py
@@ -2,37 +2,31 @@
 import unittest
 
 
-
-
-
 import numpy as np
 
 
 from demod.metrics.states import get_states_durations, get_durations_by_states
 
 
-
-
 class TestDurations(unittest.TestCase):
     # tests the differents helper functions on different set
     def test_get_states_duration(self):
         # test state durations
-        test_array = np.array([
-            [ 1, 1, 0, 2],
-            [ 1, 1, 1, 2],
-            [ 1, 1, 1, 1]]) # pattern all same
+        test_array = np.array(
+            [[1, 1, 0, 2], [1, 1, 1, 2], [1, 1, 1, 1]]
+        )  # pattern all same
 
         durations, durations_labels = get_states_durations(test_array)
-        self.assertTrue(np.all(durations == np.array([2,1,1,3,1,4])))
-        self.assertTrue(np.all(durations_labels == np.array([1,0,2,1,2,1])))
+        self.assertTrue(np.all(durations == np.array([2, 1, 1, 3, 1, 4])))
+        self.assertTrue(
+            np.all(durations_labels == np.array([1, 0, 2, 1, 2, 1]))
+        )
 
     def test_get_durations_by_states(self):
         # test state durations
-        test_array = np.array([
-            [ 1, 1, 0, 2],
-            [ 1, 1, 2, 2],
-            [ 1, 1, 1, 1],
-            [ 1, 5, 5, 5]])
+        test_array = np.array(
+            [[1, 1, 0, 2], [1, 1, 2, 2], [1, 1, 1, 1], [1, 5, 5, 5]]
+        )
 
         dic = get_durations_by_states(test_array)
         self.assertTrue(np.all(dic[0] == np.array([1])))
@@ -61,13 +55,13 @@ class TestDurations(unittest.TestCase):
         self.assertTrue(np.all(durations == np.array([2, 2])))
         self.assertTrue(np.all(durations_labels == np.array([0, 1])))
 
-
     def test_small_arrays(self):
         test_array = np.array([[0, 1], [1, 1]])
         dic = get_durations_by_states(test_array)
         print(dic)
         self.assertTrue(np.all(dic[0] == np.array([1])))
         self.assertTrue(np.all(dic[1] == np.array([1, 2])))
+
 
 #   def test_24_hour_occupancy(self):
 #       # test 24 hour occupancy
@@ -83,6 +77,5 @@ class TestDurations(unittest.TestCase):
 #       self.assertTrue(np.all(occupancy_24 == np.array([False, False, True, True, False])))
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
